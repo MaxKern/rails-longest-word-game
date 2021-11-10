aef05e72bdc8d5f791ada0c392754147ff0b4fbd
Max Kern <m.kern@ingwiest.de>
2021-11-10 11:18:07 +0000

    Longest word quiz - first steps added

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,11 @@
+class GamesController < ApplicationController
+  def new
+    alphabet = ('A'..'Z').to_a
+    @letters = []
+    10.times { @letters << alphabet.sample }
+  end
+
+  def score
+    @letters = params[:letters].split
+  end
+end
